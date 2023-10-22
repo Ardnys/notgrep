d874255b6bcb3df38d2052f51a0813fa66b33001
Ardnys <semresavas35@gmail.com>
2023-10-22 16:37:50 +0300

    insert nodes alphabetically

--- src/main/java/searchengine/WordList.java
+++ src/main/java/searchengine/WordList.java
@@ -4,18 +4,42 @@ import java.util.List;
 
 public class WordList {
     public WordListNode root;
-    private WordListNode last;
 
     public WordList() {    }
 
     public void append(WordListNode node) {
+        // TODO this should be alphabetic
         if (root == null) {
             root = node;
         } else {
-            last.next = node;
+            insert(node);
         }
-        last = node;
     }
+    private void insert(WordListNode node) {
+        WordListNode current = root;
+        while (current != null) {
+            int wordComparison = node.getWord().compareTo(current.getWord());
+            if (wordComparison > 0) {
+                if (current.next != null) {
+                    if (node.getWord().compareTo(current.next.getWord()) < 0) {
+                        // found the place
+                        WordListNode hold = current.next;
+                        current.next = node;
+                        node.next = hold;
+                        return;
+                    }
+                } else {
+                    current.next = node;
+                    return;
+                }
+            } else {
+                // duplicate
+                return;
+            }
+            current = current.next;
+        }
+    }
+
     public void printList() {
         WordListNode current = root;
         System.out.print("head->");
